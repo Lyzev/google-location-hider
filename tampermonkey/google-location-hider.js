a53efe40ffa4fea7734f3b4e1ceb3d4c7d386f88
Lyzev <lyzev.info@gmail.com>
2023-02-03 20:53:24 +0100

    - Fixed icon in tampermonkey script
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- tampermonkey/google-location-hider.js
+++ tampermonkey/google-location-hider.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Google Location Hider
-// @version      1.0.1
+// @version      1.0.2
 // @description  Removes the footer of the Google page that would otherwise show your location.
 // @author       Lyzev
 // @match       *://*.google.com/*
-// @icon         https://github.com/Lyzev/google-location-hider/raw/master/assets/img/icon-128x128.png
+// @icon         https://github.com/Lyzev/google-location-hider/raw/master/tampermonkey/img/icon-128x128.png
 // @grant        none
 // ==/UserScript==
 document.getElementById("footcnt")?.remove()
