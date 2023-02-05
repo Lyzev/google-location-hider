18fc4fe5beb0cb6bd10ac3037c2cf8d04146dc80
Lyzev <lyzev.info@gmail.com>
2023-02-05 13:44:34 +0100

    Fixed hashing
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

---
+++ release/google-location-hider.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Google Location Hider
+// @version      1.0.3
+// @description  Removes the footer of the Google page that would otherwise show your location.
+// @author       Lyzev
+// @match       *://*.google.com/*
+// @icon         https://github.com/Lyzev/google-location-hider/raw/master/tampermonkey/img/icon-128x128.png
+// @grant        none
+// ==/UserScript==
+document.getElementById("footcnt")?.remove()
